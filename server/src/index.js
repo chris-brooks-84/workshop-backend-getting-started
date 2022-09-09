4df00a83ba08a888c12d20799286de011f27ac36
Megan Sullivan <meganesulli@gmail.com>
2022-09-09 16:23:54 -0700

    feat: add starter code for server index (using mocks)

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,18 @@
-// TODO
+const { ApolloServer, gql } = require('apollo-server');
+const { readFileSync } = require('fs');
+const mocks = require('./mocks');
+
+const typeDefs = gql(readFileSync('./src/schema.graphql', { encoding: 'utf-8' }));
+
+const server = new ApolloServer({
+  typeDefs,
+  mocks
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ port, url }) => {
+  console.log(`
+    🚀  Server is running
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
+  `);
+});
